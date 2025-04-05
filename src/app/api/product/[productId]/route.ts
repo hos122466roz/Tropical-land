70f9e2253a223fe5c2c567ca9066bd8883b32035
Hossein Roozdar <hoesin122466@gmail.com>
2025-04-05 23:33:31 +0330

    updated api

--- src/app/api/product/[productId]/route.ts
+++ src/app/api/product/[productId]/route.ts
@@ -2,16 +2,16 @@ import getCurrentUser from "@/app/actions/getCurrentUser";
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 
-interface Iparams {
-  productId: string ;
+interface IParams {
+  productId: string;
 }
-export async function DELETE(request: Request, { params }: { params: Iparams }) {
+export async function DELETE(request: Request, { params }: { params: IParams }) {
   const currnetUser = await getCurrentUser();
   if (!currnetUser) return NextResponse.error();
   const { productId } = params;
-      if (!productId || typeof productId !== "string") {
-        throw new Error("Invalid ID");
-      }
+  if (!productId || typeof productId !== "string") {
+    throw new Error("Invalid ID");
+  }
   const deleteProduct = await prisma.product.deleteMany({
     where: {
       id: productId,
