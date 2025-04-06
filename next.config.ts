c2c348b6f0fadb286de9e2a2686c69f0d762a06f
Hossein Roozdar <hoesin122466@gmail.com>
2025-04-06 17:50:35 +0330

    updated

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,9 @@ const nextConfig: NextConfig = {
   /* config options here */
   images: {
     domains: ["avatars.githubusercontent.com", "res.cloudinary.com", "utfs.io"],
-  
-    
   },
+
+  missingSuspenseWithCSRBailout: false,
 };
 
 export default nextConfig;
