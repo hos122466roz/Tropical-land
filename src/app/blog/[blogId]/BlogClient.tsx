d69da10d59f547f6b6e1d90d28caef3b7ff93964
Hossein Roozdar <hoesin122466@gmail.com>
2025-04-06 12:19:34 +0330

    updated

--- src/app/blog/[blogId]/BlogClient.tsx
+++ src/app/blog/[blogId]/BlogClient.tsx
@@ -1,4 +1,4 @@
-import { SafeBlog, SafeUser } from "@/app/types"
+import { SafeBlog, SafeUser } from "@/app/types";
 import { format } from "date-fns-jalali";
 import { CiUser } from "react-icons/ci";
 
@@ -10,41 +10,42 @@ interface BlogClientProps {
   };
 }
 
-const BlogClient: React.FC<BlogClientProps>=({blog})=>{
-    return (
-      <div className="w-full   ">
-        <div className="relative">
-          <img src={blog?.cover} className="w-full  object-cover" />
-          <div className="bg-white block absolute p-1 right-6  -bottom-4  shadow-md ">
-            <h3 className="font-Dana px-2 py-1 text-black text-[12px] ">
-              {format(blog.createdAt, "yyyy MMMM d")}
-            </h3>
-          </div>
+const BlogClient: React.FC<BlogClientProps> = ({ blog }) => {
+  if(!blog) return <h1>بلاگ مورد نظر پیدا نشد</h1>
+  return (
+    <div className="w-full   ">
+      <div className="relative">
+        <img src={blog?.cover} className="w-full  object-cover" />
+        <div className="bg-white block absolute p-1 right-6  -bottom-4  shadow-md ">
+          <h3 className="font-Dana px-2 py-1 text-black text-[12px] ">
+            {format(blog.createdAt, "yyyy MMMM d")}
+          </h3>
         </div>
-        <div className=" my-12">
-          <h2 className="font-Dana-medium line-clamp-1  text-zinc-900 text-[20px]">
-            {blog.title}
-          </h2>
-          <div className="mt-7 flex items-center  py-2 border-t border-b border-gray-700/10  ">
-            <div className="flex items-center justify-center font-Dana gap-2  text-gray-700">
-              <span>
-                <CiUser size={18} />
-              </span>
-              <span>ادمین</span>
-            </div>
-            <div className="flex items-center justify-center font-Dana gap-2 mr-6  text-gray-700">
-              <span>
-                <LiaCommentsSolid size={18} />
-              </span>
-              <span>20</span>
-            </div>
-          </div>
-        </div>
-        <div
-          dangerouslySetInnerHTML={{ __html: blog?.description }}
-          className=" [&_h2]:text-2xl [&>span]:bg-red-500 *:font-Dana"
-        ></div>
       </div>
-    );
-}
-export default BlogClient
+      <div className=" my-12">
+        <h2 className="font-Dana-medium line-clamp-1  text-zinc-900 text-[20px]">
+          {blog.title}
+        </h2>
+        <div className="mt-7 flex items-center  py-2 border-t border-b border-gray-700/10  ">
+          <div className="flex items-center justify-center font-Dana gap-2  text-gray-700">
+            <span>
+              <CiUser size={18} />
+            </span>
+            <span>ادمین</span>
+          </div>
+          <div className="flex items-center justify-center font-Dana gap-2 mr-6  text-gray-700">
+            <span>
+              <LiaCommentsSolid size={18} />
+            </span>
+            <span>20</span>
+          </div>
+        </div>
+      </div>
+      <div
+        dangerouslySetInnerHTML={{ __html: blog?.description }}
+        className=" [&_h2]:text-2xl [&>span]:bg-red-500 *:font-Dana"
+      ></div>
+    </div>
+  );
+};
+export default BlogClient;
