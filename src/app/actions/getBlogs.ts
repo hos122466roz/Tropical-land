82a57c3f72fbfecb01adab7bc52460021800c958
Hossein Roozdar <hoesin122466@gmail.com>
2025-04-06 11:47:36 +0330

    updated action file

--- src/app/actions/getBlogs.ts
+++ src/app/actions/getBlogs.ts
@@ -2,10 +2,10 @@ import prisma from '@/app/libs/prismadb'
 export default async function getBlogs(){
 try{
 const blog=await prisma.blog.findMany()
-const safeBlogs=blog.map(blog=>({
-    ...blog,
-    createdAt:blog.createdAt.toISOString()
-}))
+const safeBlogs = blog.map((blogitems:any) => ({
+  ...blogitems,
+  createdAt: blogitems.createdAt.toISOString(),
+}));
 return safeBlogs
 }catch (error:any){
     throw new Error(error)
