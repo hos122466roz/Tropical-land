6db0b5ccce1695ab8d334d7e92c8687f9059ab28
Hossein Roozdar <hoesin122466@gmail.com>
2025-04-06 11:50:54 +0330

    updated action file 2

--- src/app/actions/getProduct.ts
+++ src/app/actions/getProduct.ts
@@ -2,7 +2,7 @@ import prisma from '@/app/libs/prismadb'
 export default async function getProduct(){
     try{
         const products=await prisma.product.findMany()
-         const safeProducts = products.map((product) => ({
+         const safeProducts = products.map((product:any) => ({
            ...product,
            createAt: product.createdAt.toISOString(),
          }));
